04dc21d93ecc2c1e3a11ef4d62e3127920c177ef
Dmytro-Ivanenko <dmitro0399@gmail.com>
2023-05-21 18:36:06 +0300

    add send status

--- controllers/payControllers.js
+++ controllers/payControllers.js
@@ -1,6 +1,10 @@
 const { customAlphabet } = require('nanoid');
+var base64 = require('base-64');
+var utf8 = require('utf8');
+
 const LiqPay = require('../utils/liqpay');
 const { ctrlWrapper } = require('../decorators');
+const { sendEmail } = require('../utils');
 require('dotenv').config();
 
 const { PUBLIC_KEY, PRIVATE_KEY } = process.env;
@@ -36,7 +40,79 @@ const getKeys = async (req, res) => {
 	res.status(200).json(dataForPay);
 };
 
+// response from liqpay with pay status
+const payStatus = async (req, res) => {
+	const { data, signature } = req.body;
+	const str = PRIVATE_KEY + data + PRIVATE_KEY;
+	const mySign = liqpay.str_to_sign(str);
+
+	if (mySign !== signature) {
+		return;
+	}
+
+	const bytes = base64.decode(data);
+	const decData = JSON.parse(utf8.decode(bytes));
+
+	const {
+		order_id,
+		status,
+		amount,
+		completion_date,
+		create_date,
+		description,
+		end_date,
+		err_code,
+		err_description,
+		ip,
+		is_3ds,
+		liqpay_order_id,
+		payment_id,
+		paytype,
+		receiver_commission,
+		sender_card_mask2,
+		sender_card_type,
+		sender_commission,
+		sender_first_name,
+		sender_last_name,
+		sender_phone,
+	} = decData;
+
+	const messageData = {
+		to: 'd.ivanenko@ukr.net',
+		subject: `Rozkvitay замовлення номер: ${order_id}  статус: ${status}`,
+		html: `
+<p>order_id: ${order_id}</p>
+<p>status: ${status}</p>
+<p>amount: ${amount}</p>
+<p>completion_date: ${completion_date}</p>
+<p>create_date: ${create_date}</p>
+<p>description: ${description}</p>
+<p>end_date: ${end_date}</p>
+<p>err_code: ${err_code}</p>
+<p>err_description: ${err_description}</p>
+<p>ip: ${ip}</p>
+<p>is_3ds: ${is_3ds}</p>
+<p>liqpay_order_id: ${liqpay_order_id}</p>
+<p>payment_id: ${payment_id}</p>
+<p>paytype: ${paytype}</p>
+<p>receiver_commission: ${receiver_commission}</p>
+<p>sender_card_mask2: ${sender_card_mask2}</p>
+<p>sender_card_type: ${sender_card_type}</p>
+<p>sender_commission: ${sender_commission}</p>
+<p>sender_first_name: ${sender_first_name}</p>
+<p>sender_last_name: ${sender_last_name}</p>
+<p>sender_phone: ${sender_phone}</p>`,
+	};
+
+	await sendEmail(messageData);
+
+	res.status(200).json({
+		decData,
+	});
+};
+
 module.exports = {
 	initConnection: ctrlWrapper(initConnection),
 	getKeys: ctrlWrapper(getKeys),
+	payStatus: ctrlWrapper(payStatus),
 };
--- routes/api/pay.js
+++ routes/api/pay.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const { validateBody } = require('../../utils');
 const { schemas } = require('../../models/body.js');
-const { getKeys, initConnection } = require('../../controllers/payControllers');
+const {
+	getKeys,
+	initConnection,
+	payStatus,
+} = require('../../controllers/payControllers');
 
 const router = express.Router();
 
 // routes
 router.get('/init', initConnection);
 router.post('/keys', validateBody(schemas.bodyValidateSchema), getKeys);
+router.post('/status', payStatus);
 
 // export
 module.exports = router;
